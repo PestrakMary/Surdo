4d706d1c27ff61c6417a8aa623af14d40f34ed1f
Mary <mari.pestrak.99@mail.ru>
2020-12-12 17:07:39 +0300

    update dependencies

--- app/src/main/java/com/example/surdo/MainActivity.java
+++ app/src/main/java/com/example/surdo/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.surdo;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
