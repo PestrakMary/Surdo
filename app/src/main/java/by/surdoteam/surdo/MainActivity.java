449435eb97e56f501891dc21731a8b1826c12671
Mary Pestrak <mary.pestrak.99@mail.ru>
2021-04-08 22:33:16 +0300

    fix menu

--- app/src/main/java/by/surdoteam/surdo/MainActivity.java
+++ app/src/main/java/by/surdoteam/surdo/MainActivity.java
@@ -124,7 +124,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
