4f265dc286e659c869ab3f950b98088416bb2a21
uvlad7 <uvlad7@gmail.com>
2021-06-05 21:34:04 +0300

    Fix about: rename textviews, fix links

--- app/src/main/java/by/surdoteam/surdo/fragments/AboutFragment.java
+++ app/src/main/java/by/surdoteam/surdo/fragments/AboutFragment.java
@@ -2,9 +2,11 @@ package by.surdoteam.surdo.fragments;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -19,7 +21,10 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        ((TextView) view.findViewById(R.id.textViewAboutText)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView) view.findViewById(R.id.textViewMentioning)).setMovementMethod(LinkMovementMethod.getInstance());
+        return view;
     }
 
     @Override
